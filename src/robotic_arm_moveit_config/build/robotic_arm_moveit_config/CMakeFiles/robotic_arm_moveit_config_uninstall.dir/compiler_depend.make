f433a1eec3169dc0c17c5fe3915c6f4c553cd280
stardust <2409602366@qq.com>
2025-01-08 18:50:12 +0800

    switched to the right model

--- src/robotic_arm_moveit_config/build/robotic_arm_moveit_config/CMakeFiles/robotic_arm_moveit_config_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for robotic_arm_moveit_config_uninstall.
-# This may be replaced when dependencies are built.
--- src/robotic_arm_moveit_config/build/robotic_arm_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/engineer/code/rm_eng/src/robotic_arm_moveit_config
-# Build directory: /home/engineer/code/rm_eng/src/robotic_arm_moveit_config/build/robotic_arm_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
