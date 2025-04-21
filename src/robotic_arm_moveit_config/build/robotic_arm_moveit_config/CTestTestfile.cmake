4aa2e99b4621c225b77ed6adf0803f0b3e4ca8a6
stardust <2409602366@qq.com>
2025-04-21 20:46:18 +0800

    simplified model

---
+++ src/robotic_arm_moveit_config/build/robotic_arm_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/engineer/code/robotic_arm_moveit_config
+# Build directory: /home/engineer/code/robotic_arm_moveit_config/build/robotic_arm_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
